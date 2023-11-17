75e4bf8039633014547392b27769dcd395c0c9b3
jl-bernal <jlbernal@ifca.unican.es>
2023-11-17 11:18:24 +0100

    adding a general L(M) relation linear with SFR

--- source/mass_luminosity.py
+++ source/mass_luminosity.py
@@ -333,6 +333,32 @@ def SilvaLyalpha_12(self,Mvec,MLpar,z):
     
     return (Lrec+Lexc+Lcool+Lcont).to(u.Lsun)
     
+def KSrel(self,Mvec,MLpar,z):
+    '''
+    Uses the Kennicutt-Schmidt relation: linear with SFR, adding an
+    absorption term.
+    L = K*SFR*10^{-Aext/2.5}
+    Parameters:
+    K_Halpha     normalization between SFR and L -> With units!!
+    Aext         Extinction
+    SFR_file     file with SFR
+    '''
+    K = MLpar['K']
+    Aext = MLpar['Aext']
+    SFR_file = MLpar['SFR_file']
+    
+    SFR = get_SFR(Mvec,z,SFR_file)
+    #is there quenching?
+    try:
+        do_quench = ML['do_quench']
+        if do_quench:
+            fQint = process_fq()
+            SFR *= (1-fQint(np.log10(Mvec.value),1+z))
+    except:
+        pass
+            
+    L = SFR*K*10**(-Aext/2.5)
+    return L.to(u.Lsun)
 
 def GongHalpha(self,Mvec,MLpar,z):
     '''
